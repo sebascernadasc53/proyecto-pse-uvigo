cb7ab9a06e29b79384830db8b5d4ecbf249ec8dd
Sebastián Carlos Cernadas Cernadas <sebastiancernadas@ingenierosvigo.com>
2026-01-30 14:07:56 +0100

    Update antichoque.py
    modificación para que utilice la clase creada en robot.py (Falta probar que funcione)

--- antichoque.py
+++ antichoque.py
@@ -1,52 +1,31 @@
-from motor import Ordinary_Car
-from ultrasonic import Ultrasonic
 import time
 from threading import Thread
+from robot import Robot
 
 # Inicialización
-ultrasonic = Ultrasonic()
-PWM = Ordinary_Car()
-distance = 0
-running = True  # Flag de control global
+rover1 = Robot()
+dist = 0
+v=1000
 
-def leer_ultrasonidos():
-    global distance
-    while running: # El hilo se detiene si running es False
-        distance = ultrasonic.get_distance()
-        if distance is not None:
-            print(f"Distancia ultrasonidos {distance} cm")
-        time.sleep(0.05) # Evita saturar el bus
+print("Prueba...")
 
-def motores():
-    while running: # El hilo se detiene si running es False
-        if distance > 90:
-            PWM.set_motor_model(-4095, -4095, -4095, -4095) # mínimo para que arranque 600
-        elif 25 < distance <= 90:
-            PWM.set_motor_model(-400, -400, -400, -400)
-        else:
-            PWM.set_motor_model(0, 0, 0, 0)
-        time.sleep(0.05) # Crucial para soltar el GIL y permitir el cierre
-
-try: 
-    sensor_thread = Thread(target=leer_ultrasonidos, daemon=True)
-    motor_thread = Thread(target=motores, daemon=True)
-
-    sensor_thread.start()
-    motor_thread.start()
-
+try:
     while True:
-        time.sleep(1)
+        dist = rover1.distance
+        
+        if dist > 90:
+            v=v
+        elif 25 < dist <= 90:
+            v=0.6*v
+        else:
+            print(f"¡Objeto detectado! Frenando...")
+            v=0
+            
+        rover1.motores(v)
+        print(f"d={dist} cm")
+        time.sleep(0.1)
 
 except KeyboardInterrupt:
-    print("\n[!] Deteniendo sistemas...")
-    running = False  # 1. Ordenamos a los hilos detener sus bucles
-    time.sleep(0.2)  # 2. Esperamos un instante a que los hilos terminen
-    PWM.set_motor_model(0, 0, 0, 0) # 3. Enviamos la orden final de parada
-
+    print("\nFinalizando ejecución...")
 finally:
-    running = False
-    PWM.set_motor_model(0, 0, 0, 0)
-    # Intentar limpiar la librería si tiene el método
-    if hasattr(PWM, 'close'):
-        PWM.close()
-    print("Programa finalizado con éxito.")
+    rover1.finalizar()
