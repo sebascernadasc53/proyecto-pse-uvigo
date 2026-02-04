af8a6c25699646dcf2785bd17cafeafe731065ed
Sebastián Carlos Cernadas Cernadas <sebastiancernadas@ingenierosvigo.com>
2026-02-04 11:35:12 +0100

    Update antichoque_sim.py
    añadido el código

---
+++ sim/antichoque_sim.py
@@ -0,0 +1,31 @@
+import time
+from robot_sim import Robot
+
+# Inicialización
+rover1 = Robot()
+dist = 0
+v0=1000
+
+print("Prueba...")
+
+try:
+    while True:
+        dist = rover1.distance
+        
+        if dist > 90:
+            v=v0
+            print(f"d={dist} cm Movimiento")
+        elif 25 < dist <= 90:
+            v=0.6*v0
+            print(f"d={dist} cm Movimiento Lento")
+        else:
+            print(f"d={dist} cm ¡Objeto detectado! Frenando...")
+            v=0
+            
+        rover1.motores(v)
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("\nFinalizando ejecución...")
+finally:
+    rover1.finalizar()
