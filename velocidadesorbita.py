5ba028c00135c17f9841e598311fe58754e18362
martinferreiraprez <martinferreiraperez@gmail.com>
2026-02-05 10:33:20 +0100

    completo primera calibración

--- velocidadesorbita.py
+++ velocidadesorbita.py
@@ -6,15 +6,17 @@ from ultrasonic import Ultrasonic
 ultrasonic = Ultrasonic()
 PWM = Ordinary_Car()
 dist = ultrasonic.get_distance()
+PWM.set_motor_model(0,0,0,0)
 try:
-     while dist < 50:
-          print("Probando órbitas horarias, para antihorarias cambiar")
+     while dist > 5:
+          print("Probando órbitas horarias, para antihorarias cambiar signo")
           print(f"Distancia: {dist} cm")
-          print("Probando órbita horaria. Peso Traslación = Peso Rotación")
+          print("Peso Traslación = Peso Rotación")
           PWM.set_motor_model(0,2000,0,-2000)
           print("0,2000,0,-2000")
           time.sleep(2)
-          print(f"Distancia: {dist} cm")
+
+          #ROTACIÓN
           print("Más peso a rotación, orbita más cerrada")
           PWM.set_motor_model(1000,2000,-1000,-2000)
           print("1000,2000,-1000,-2000")
@@ -23,4 +25,36 @@ try:
           PWM.set_motor_model(1000,3000,-1000,-3000)
           print("1000,3000,-1000,-3000")
           time.sleep(2)
+          print("Otros valores 2")
+          PWM.set_motor_model(2000,4000,-2000,-4000)
+          print("2000,4000,-2000,-4000")
+          time.sleep(2)
+          PWM.set_motor_model(0,0,0,0)
+          print("COCHE PARADO, APROVECHA PARA RECOLOCARLO SI HAY POCO ESPACIO, 10 SECS")
+          time.sleep(10)
+
+          #TRANSLACIÓN
+          print("Mayor peso en translación,órbitas más abiertas")
+          PWM.set_motor_model(-1000,2000,-1000,2000)
+          print("0,2000,0,-2000")
+          time.sleep(2)
+          print("Otros valores")
+          PWM.set_motor_model(-1000,2000,1000,-2000)
+          print("-1000,2000,1000,-2000")
+          time.sleep(2)
+          print("Otros valores 2")
+          PWM.set_motor_model(-2000,1000,1000,-3000)
+          print("-2000,1000,1000,-3000")
+          time.sleep(2)
+
+          
+          
+          
+
+
+
+except KeyboardInterrupt:
+    PWM.set_motor_model(0,0,0,0)
+    print("Deteniendo calibración")
+            
           
