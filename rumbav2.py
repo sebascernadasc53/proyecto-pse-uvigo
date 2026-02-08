637fc57566aa8abaf1ba60de3639134f1bdf5571
AdrianCarreraM <adrian.carrera@alumnado.uvigo.gal>
2026-02-08 20:22:51 +0100

    Update rumbav2.py
    Cambios

--- rumbav2.py
+++ rumbav2.py
@@ -1,6 +1,9 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import time
 from threading import Thread, Lock
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 from ultrasonic import Ultrasonic
@@ -8,6 +11,7 @@ from servo import Servo
 from moves import Moves  
 import time
 
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
 # VARIABLES GLOBALES
 distance_front = 150 
@@ -157,6 +161,67 @@ class Car:
             self.run_motor_ultrasonic(self.car_sonic_distance)
 >>>>>>> Stashed changes
 
+=======
+class Car:
+    def __init__(self):
+        # Inicialización de hardware
+        self.servo = Servo()
+        self.sonic = Ultrasonic()
+        self.motor = Moves()  
+
+        # Variables de control
+        self.car_record_time = time.time()
+        self.car_sonic_servo_angle = 30
+        self.car_sonic_servo_dir = 1
+        self.car_sonic_distance = [30, 30, 30]
+
+    def close(self):
+        # Parada segura
+        self.motor.stop()
+        self.sonic.close()
+        self.motor.close()
+        self.servo = None
+        self.sonic = None
+        self.motor = None
+
+    # ===== DECISIÓN DE MOVIMIENTO =====
+    def run_motor_ultrasonic(self, distance):
+        left, front, right = distance
+
+        # --- Zona de parada y giro ---
+        if front < 30 or (left < 30 and right < 30):
+            # Parar y girar a la derecha (spin horario)
+            self.motor.clockwise_turn(speed=500)
+
+        # --- Zona de reducción de velocidad ---
+        elif front < 50 or left < 50 or right < 50:
+            # Avanza pero lento
+            self.motor.forward(speed=300)
+
+        # --- Camino libre ---
+        else:
+            self.motor.forward(speed=600)
+
+    # ===== BARRIDO DEL ULTRASONIDO Y DECISIÓN =====
+    def mode_ultrasonic(self):
+        if (time.time() - self.car_record_time) > 0.2:
+            self.car_record_time = time.time()
+
+            # Mueve servo al ángulo actual
+            self.servo.set_servo_pwm('0', self.car_sonic_servo_angle)
+
+            # Guardar la distancia según el ángulo
+            if self.car_sonic_servo_angle == 30:
+                self.car_sonic_distance[0] = self.sonic.get_distance()
+            elif self.car_sonic_servo_angle == 90:
+                self.car_sonic_distance[1] = self.sonic.get_distance()
+            elif self.car_sonic_servo_angle == 150:
+                self.car_sonic_distance[2] = self.sonic.get_distance()
+
+            # Llamar a la función de movimiento
+            self.run_motor_ultrasonic(self.car_sonic_distance)
+
+>>>>>>> Stashed changes
             # Actualizar dirección de barrido del servo
             if self.car_sonic_servo_angle <= 30:
                 self.car_sonic_servo_dir = 1
@@ -173,6 +238,7 @@ class Car:
 def test_car_sonic():
     car = Car()
     try:
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
         t_radar.start()
         t_robot.start()
@@ -195,5 +261,13 @@ def test_car_sonic():
         print("\nEnd of program")
 >>>>>>> Stashed changes
 
+=======
+        while True:
+            car.mode_ultrasonic()
+    except KeyboardInterrupt:
+        car.close()
+        print("\nEnd of program")
+
+>>>>>>> Stashed changes
 if __name__ == '__main__':
     test_car_sonic()
