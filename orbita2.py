96b244bd86c4a4103b5ac23cec1ba2903b09e8a0
martinferreiraprez <martinferreiraperez@gmail.com>
2026-02-08 20:16:23 +0100

    añado threading

--- orbita2.py
+++ orbita2.py
@@ -1,22 +1,32 @@
 import time
 from motor import Ordinary_Car 
 from ultrasonic import Ultrasonic
+from threading import Thread
 
 ultrasonic = Ultrasonic()
 PWM = Ordinary_Car()
-
+distance = 0.0
 D_TARGET = 40  # Distancia deseada al objeto (cm)
 TOL = 5
 Speed = [-400,4000,400,-4000]
+running = True
+
+ # HILO SENSOR
+def sensor_thread():
+    global distance
+    while running:
+        lectura = ultrasonic.get_distance()
+        if lectura is not None:
+            distance = lectura
+        time.sleep(0.05)
 def verificar_objeto():
     print("Analizando orbitabilidad...")
     lecturas = []
     for _ in range (5):
         PWM.set_motor_model(Speed[0], Speed[1], Speed[2], Speed[3]) #orbita
         time.sleep(0.3)
-        d = ultrasonic.get_distance()
-        if d: lecturas.append(d)
-        time.sleep(0.5)
+        lecturas.append(distance)
+        time.sleep(0.2)
     if len(lecturas) < 3:
         print(f"No hay suficientes lecturas, {(len(lecturas))},para verificar el objeto")
         return False
@@ -28,14 +38,19 @@ def verificar_objeto():
         return True
 def orbitar_objeto():
     while True:
-        d = ultrasonic.get_distance()
         PWM.set_motor_model(Speed[0], Speed[1], Speed[2], Speed[3])
-        if d >=D_TARGET + 2*TOL or d < D_TARGET - 2*TOL:
+        if distance >=D_TARGET + 2*TOL or distance < D_TARGET - 2*TOL:
             print("Órbita perdida")
             PWM.set_motor_model(0,0,0,0)
             break
-        
-try: #bucle principal, buscar objeto
+        time.sleep(0.1)
+
+t = Thread(target=sensor_thread)
+t.daemon = True
+t.start()
+
+#bucle principal, buscar objeto
+try:
     while True:
         d = ultrasonic.get_distance()
         if d > D_TARGET + TOL:
@@ -48,5 +63,6 @@ try: #bucle principal, buscar objeto
             if verificar_objeto():
                 orbitar_objeto()
 except KeyboardInterrupt:
+    running = False
     print("Abortando programa")
     PWM.set_motor_model(0,0,0,0)
