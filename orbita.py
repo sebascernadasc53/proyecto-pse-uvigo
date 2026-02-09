9e767db8b0240b344bb4b726ccccc53a9fd2c06b
martinferreiraprez <martinferreiraperez@gmail.com>
2026-02-09 19:13:21 +0100

    bugfix

--- orbita.py
+++ orbita.py
@@ -1,15 +1,16 @@
 import time
-from motor import Ordinary_Car 
+#from motor import Ordinary_Car 
 from threading import Thread
 from robot import Robot
-
-ultrasonic = Ultrasonic()
-PWM = Ordinary_Car()
+distance = 150
+#ultrasonic = Ultrasonic()
+#PWM = Ordinary_Car()
 D_TARGET = 50  # Distancia deseada al objeto (cm)
 TOL = 10
 Speed = [-1200,4000,800,-4000]
 running = True
 robot = Robot()
+
  # HILO SENSOR
 def sensor_thread():
     global distance
@@ -18,17 +19,19 @@ def sensor_thread():
         if lectura is not None:
             distance = lectura
         time.sleep(0.05)
+
+# VERIFICACION
 def verificar_objeto():
     print("Analizando orbitabilidad...")
-    robot.stop
+    robot.stop()
     lecturas = []
-    for _ in range (10):
-        Robot.counter_clockwise_orbit
-        #PWM.set_motor_model(Speed[0], Speed[1], Speed[2], Speed[3]) #orbita
-        robot.stop
+    for _ in range (5):
+        robot.counter_clockwise_orbit()
+        '''PWM.set_motor_model(Speed[0], Speed[1], Speed[2], Speed[3])'''
+        time.sleep(0.3)
+        robot.stop()
         time.sleep(0.3)
         lecturas.append(distance)
-        time.sleep(0.3)
         
     if len(lecturas) < 3:
         print(f"No hay suficientes lecturas, {(len(lecturas))},para verificar el objeto")
@@ -39,17 +42,19 @@ def verificar_objeto():
     else:
         print("Orbitable")
         return True
+#ORBITA
 def orbitar_objeto():
-    for _ in (5):
-        print("Orbitando")
-        robot.counter_clockwise_orbit
-        #PWM.set_motor_model(Speed[0], Speed[1], Speed[2], Speed[3])
-        if distance >=D_TARGET + 2*TOL or distance < D_TARGET - 2*TOL:
-            print("Órbita perdida")
-            Robot.stop
-            break
-        time.sleep(0.1)
+    
+    print("Orbitando")
+    robot.counter_clockwise_orbit()
+    #PWM.set_motor_model(Speed[0], Speed[1], Speed[2], Speed[3])
+    if distance >=D_TARGET + 2*TOL or distance < D_TARGET - 2*TOL:
+        print("Órbita perdida")
+        robot.stop()
+    time.sleep(0.1)
+# MAIN
 def main():
+    global running
     t = Thread(target=sensor_thread)
     t.daemon = True
     t.start()
@@ -62,17 +67,18 @@ def main():
                 print("Buscando objetivo")
 
             elif distance > D_TARGET + 2*TOL:
-                PWM.set_motor_model(-300,-300,-300,-300)
+                robot.forward(300)
 
             elif distance < D_TARGET - 2*TOL:
-                PWM.set_motor_model(500,500,500,500)
+                robot.backward(500)
                 print("Demasiado cerca, retrocediendo")
             else:
                 if verificar_objeto():
                     orbitar_objeto()
+            time.sleep(0.05)
     except KeyboardInterrupt:
         running = False
         print("Abortando programa")
-        robot.stop
+        robot.stop()
 if __name__ == "__main__":
     main()
